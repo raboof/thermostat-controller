57a45795dd8e06b78b9034bce476f5c068b9f5b6
Renate Bresser <renatebresser@hotmail.com>
2015-12-22 22:56:51 +0100

    introduce heating time that is learned over time
    This algorithm should start with warming up according to the difference between temperature and target times the heating up time, then wait for the temperature to come down again so you know the top of the curve is reached. Evaluate and addapt heating time.

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 var http = require('http');
 var gpio = require('gpio');
 
+var previous_temp;
 var temperature;
 var target;
 
-var old_temp;
-var new_temp;
-var old_time;
-var new_time;
-var interval = 24;
-var counter = 0;
+var heating_initiated;
+var heating_time = 60;
+var started_heating_at_temp;
+var heating_started_at_time;
+var target_changed_at_time;
+var target_difference;
 
 var pin = gpio.export(process.env.HEATING_GPIO_OUT, {
   direction: 'high',
@@ -19,45 +20,59 @@ var pin = gpio.export(process.env.HEATING_GPIO_OUT, {
   }
 });
 
-function record_temperature() {
-  counter += 1;
-  if (counter == interval) {
-    old_temp = new_temp;
-    old_time = new_time;
-    new_temp = temperature;
-    new_time = Math.floor(Date.now() / 1000);
-    counter = 0;
-  }
+function get_time() {
+  return Math.floor(Date.now() / 1000);
 }
 
-function get_temp_over_time() {
-  if (old_temp && new_temp) {
-    var temp_change = new_temp - old_temp;
-    var time = new_time - old_time;
-    return temp_change / time;
-  }
-  return 0;
+function turn_heating_on() {
+  console.log("Turning on");
+  pin.set(0, function(){});
+  heating_initiated = true;
+  started_heating_at_temp = temperature;
+  target_difference = target - started_heating_at_temp;
+  started_heating_at_time = get_time();
 }
 
-function predict_reaching_target() {
-  var changing_rate = get_temp_over_time();
-  var time = 120;
-  return temperature + time * changing_rate > target;
+function turn_heating_off() {
+  console.log("Turning off");
+  pin.set(1, function(){});
+}
+
+function is_in_heating_fase() {
+  return get_time() - heating_started_at_time < target_difference * heating_time;
 }
 
 function updatePin() {
-  if (target && temperature) {
-
+  if (target && temperature && previous_temp) {
     console.log("Target: " + target + ", current: " + temperature);
-    
-    var reaching_target = predict_reaching_target();
 
-    if (reaching_target) {
-      console.log("Turning off");
-      pin.set(1, function(){});
-    } else {
-      console.log("Turning on");
-      pin.set(0, function(){});
+    if (heating_initiated) {
+      if (is_in_heating_fase()) {
+        if (target_changed_at_time > started_heating_at_time && temperature > target) {
+          turn_heating_off();
+          heating_initiated = false;
+        }
+        return;
+      } else {
+        turn_heating_off();
+        // wait for temperature drop after heating
+        if (temperature < previous_temp && temperature > started_heating_at_temp) {
+          // drop reached! end of cycle, time to evaluate.
+          heating_initiated = false;
+          var temp_reached = previous_temp;
+          var difference_reached = temp_reached - started_heating_at_temp;
+          if (temp_reached > target) {
+            heating_time = Math.floor(heating_time / difference_reached * target_difference);
+          } else if (temp_reached < target) {
+            heating_time = Math.floor(heating_time / difference_reached * target_difference);
+          }
+        } else {
+          // drop not yet reached
+          return;
+        } 
+      }
+    } else if (target > temperature) {
+      turn_heating_on();
     }
   }
 }
@@ -80,12 +95,13 @@ function monitor(url, callback) {
 
 monitor(process.env.DB_URL + '/stream/temperature', function(value) {
   console.log("got " + value + " for temperature");
+  previous_temp = temperature;
   temperature = value;
-  record_temperature();
   updatePin();
 });
 monitor(process.env.DB_URL + '/stream/target', function(value) {
   console.log("got " + value + " for target");
   target = value;
+  target_changed_at_time = get_time();
   updatePin();
 });
